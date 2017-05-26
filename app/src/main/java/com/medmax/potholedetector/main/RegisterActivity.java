a8c6d91e40e4e02bb07e3df56c6b107017bdbf81
Max Medina <maxmedina05@gmail.com>
2017-05-26 10:14:53 -0400

    Add update to location

--- app/src/main/java/com/medmax/potholedetector/main/RegisterActivity.java
+++ app/src/main/java/com/medmax/potholedetector/main/RegisterActivity.java
@@ -14,19 +14,23 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationListener;
 import com.medmax.potholedetector.R;
 
 import static com.google.android.gms.common.api.GoogleApiClient.Builder;
 import static com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 
-public class RegisterActivity extends AppCompatActivity implements OnConnectionFailedListener, ConnectionCallbacks, View.OnClickListener {
-    private static final String LOG_TAG = "RegisterActivity";
+public class RegisterActivity extends AppCompatActivity implements OnConnectionFailedListener, 
+ConnectionCallbacks, View.OnClickListener, LocationListener {
 
     // Constants
-
+    private static final String LOG_TAG = "RegisterActivity";
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    
     // Variables
     private Location mLastKnownLocation;
-
+    private LocationRequest mLocationRequest;
+    
     // GPS Interfaces
     GoogleApiClient mGoogleApiClient;
 
@@ -45,6 +49,14 @@ public class RegisterActivity extends AppCompatActivity implements OnConnectionF
         mTvLongitude = (TextView) findViewById(R.id.gps_longitude);
         mTvLatitude = (TextView) findViewById(R.id.gps_latitude);
 
+        mBtnRegister.setOnClickListener(this);
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+        
         // Create an instance of GoogleAPIClient.
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new Builder(this)
@@ -53,32 +65,55 @@ public class RegisterActivity extends AppCompatActivity implements OnConnectionF
                     .addApi(LocationServices.API)
                     .build();
         }
-
-        mBtnRegister.setOnClickListener(this);
     }
 
     @Override
-    protected void onStart() {
+    protected void onResume() {
+        super.onResume();
         mGoogleApiClient.connect();
-        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+      super.onPause();
+      if (mGoogleApiClient.isConnected()) {
+          LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+          mGoogleApiClient.disconnect();
+      }
     }
 
     @Override
     protected void onStop() {
-        mGoogleApiClient.disconnect();
+        if(mGoogleApiClient.isConnected()){
+            mGoogleApiClient.disconnect();
+        }
         super.onStop();
     }
 
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult result) {
-        Log.i(LOG_TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        Log.i(LOG_TAG, "Location Services Connected!");
-        mLastKnownLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        updateUI();
+        Log.i(LOG_TAG, "Location services connected.");
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(location == null) {
+          LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        } else {
+          handleNewLocation(location);
+        }
+        
     }
 
     @Override
@@ -86,16 +121,25 @@ public class RegisterActivity extends AppCompatActivity implements OnConnectionF
 
     }
 
-    private void updateUI() {
-        if(mLastKnownLocation != null) {
-            mTvLongitude.setText(String.valueOf(mLastKnownLocation.getLongitude()));
-            mTvLatitude.setText(String.valueOf(mLastKnownLocation.getLatitude()));
-        }
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
+    }
+
+    private void handleNewLocation(Location location) {
+      Log.d(LOG_TAG, location.toString());
+      double currentLatitude = location.getLatitude();
+      double currentLongitude = location.getLongitude();
+      updateUI(currentLatitude, currentLongitude);
+    }
+
+    private void updateUI(double latitude, double longitude) {
+        mTvLongitude.setText(String.valueOf(longitude));
+        mTvLatitude.setText(String.valueOf(latitude));
     }
 
     @Override
     public void onClick(View v) {
-        mLastKnownLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        updateUI();
+      LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
     }
 }
