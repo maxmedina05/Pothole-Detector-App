c99b21ed16f570a3a71a84d1adf0eeb67ef89d1b
Max Medina <maxmedina05@gmail.com>
2017-05-27 15:25:03 -0400

    Add SensorActivity Add accelerometer listener

--- app/src/main/java/com/medmax/potholedetector/main/SensorActivity.java
+++ app/src/main/java/com/medmax/potholedetector/main/SensorActivity.java
@@ -1,19 +1,72 @@
 package com.medmax.potholedetector.main;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.medmax.potholedetector.R;
 
-public class SensorActivity extends AppCompatActivity {
+public class SensorActivity extends AppCompatActivity implements OnClickListener, SensorEventListener {
+
+    // constants
+    public static final String LOG_TAG = SensorActivity.class.getSimpleName();
+    private int SAMPLING_RATE = SensorManager.SENSOR_DELAY_FASTEST;
+
+    // Variables
+    private boolean mIsSaving = false;
+
+    // UI Components
+    private ToggleButton mButton;
+    private TextView mTvAccAxisX;
+    private TextView mTvAccAxisY;
+    private TextView mTvAccAxisZ;
+
+    // Sensor's variables
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
+
+        mButton = (ToggleButton) findViewById(R.id.btnToggle);
+        mTvAccAxisX = (TextView) findViewById(R.id.acc_axis_x);
+        mTvAccAxisY = (TextView) findViewById(R.id.acc_axis_y);
+        mTvAccAxisZ = (TextView) findViewById(R.id.acc_axis_z);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mSensor, SAMPLING_RATE);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        mIsSaving = !mIsSaving;
+    }
+
+    // 200 hz
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 }
